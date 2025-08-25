5a03b46b881e9b55371184f5f6de06b7ec933242
Niki Brown <284173+nikibrown@users.noreply.github.com>
2025-08-25 14:48:25 -0700

    stat column layout options

--- inc/stats-section.php
+++ inc/stats-section.php
@@ -11,7 +11,12 @@ if ($background_color == "background-dark-blue" || $background_color == "backgro
 <section id="stats-<?php echo get_row_index(); ?>"
   class="stats-section <?php echo $background_color; ?> <?php echo $text_color; ?> padding-y-100">
   <span class="badge text-bg-danger">Stats</span>
+
+
+
+  <?php if (get_sub_field('stats_section_layout') == 'stats-bottom-5') : ?>
   <div class="container">
+
     <div class="row gy-4 mb-4">
       <div class="col-lg-7">
         <?php the_sub_field('left_column_content'); ?>
@@ -25,11 +30,11 @@ if ($background_color == "background-dark-blue" || $background_color == "backgro
     <div class="row flex-column flex-lg-row justify-content-center align-items-stretch gap-3">
       <?php if (have_rows('stats')) : ?>
       <?php while (have_rows('stats')) : the_row(); 
-          $stat_number = get_sub_field('stat_number');
-          $stat_unit = get_sub_field('stat_unit');
-          $stat_content = get_sub_field('stat_content');
-          $stat_text_color = get_sub_field('stat_text_color');
-        ?>
+            $stat_number = get_sub_field('stat_number');
+            $stat_unit = get_sub_field('stat_unit');
+            $stat_content = get_sub_field('stat_content');
+            $stat_text_color = get_sub_field('stat_text_color');
+          ?>
       <div class="col d-flex">
         <div class="card stat-card w-100">
           <div class="card-body d-flex flex-column">
@@ -46,11 +51,78 @@ if ($background_color == "background-dark-blue" || $background_color == "backgro
       <?php endif; ?>
 
     </div>
+    <?php if (get_sub_field('stat_bottom_cta')) : ?>
     <div class="row">
-      <div class="col-lg-8 offset-lg-2  mt-5">
+      <div class="col-lg-8 offset-lg-2 mt-5">
         <?php the_sub_field('stat_bottom_cta'); ?>
       </div>
     </div>
+    <?php endif; ?>
+  </div>
+  <?php endif; ?>
+
+
+  <?php if (get_sub_field('stats_section_layout') == 'stats-right') : ?>
+  <div class="stats-bottom-3-container">
+    <div class="container">
+      <div class="row gy-4 mb-4">
+        <div class="col-lg-8">
+          <?php the_sub_field('left_column_content'); ?>
+        </div>
+        <div class="col-lg-4">
+          <?php if (get_sub_field('right_column_content')) { ?>
+          <div class="mb-5">
+            <?php the_sub_field('right_column_content'); ?>
+          </div>
+          <?php } ?>
+
+          <div class="row">
+            <?php if (have_rows('stats')) : ?>
+            <div class="col-xl-8 offset-xl-2 col-lg-10 offset-lg-1 ">
+              <div class="d-flex flex-column gap-3">
+                <?php while (have_rows('stats')) : the_row(); 
+                  $stat_number = get_sub_field('stat_number');
+                  $stat_unit = get_sub_field('stat_unit');
+                  $stat_content = get_sub_field('stat_content');
+                  $stat_text_color = get_sub_field('stat_text_color');
+                ?>
+                <div class="card stat-card w-100">
+                  <div class="card-body d-flex flex-column">
+                    <h3 class="stat-card-title <?php echo $stat_text_color; ?>">
+                      <span class="number-flow">
+                        <number-flow class="number-flow" id="nf1" data-no-commas><?php echo $stat_number; ?>
+                        </number-flow>
+                      </span><span class="stat-card-unit"><?php echo $stat_unit; ?></span>
+                    </h3>
+                    <div class="stat-card-subtitle"><?php echo $stat_content; ?></div>
+                  </div>
+                </div>
+                <?php endwhile; ?>
+              </div>
+              <?php endif; ?>
+            </div>
+
+          </div>
+        </div>
+      </div>
+      <?php if (get_sub_field('stat_bottom_cta')) : ?>
+      <div class="row">
+        <div class="col-lg-8 offset-lg-2  mt-5">
+          <?php the_sub_field('stat_bottom_cta'); ?>
+        </div>
+      </div>
+      <?php endif; ?>
+
+    </div>
+  </div>
+  <?php endif; ?>
+
+
+
+
+
+
+
   </div>
 </section>
 
