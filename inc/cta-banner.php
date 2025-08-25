a403a6d590ec54fd755504d356f8a6ba4747375a
Niki Brown <284173+nikibrown@users.noreply.github.com>
2025-08-24 20:22:47 -0700

    Add background repeaters to cta banner, logo banner

--- inc/cta-banner.php
+++ inc/cta-banner.php
@@ -10,17 +10,27 @@ if ($background_color == "background-dark-blue" || $background_color == "backgro
 ?>
 
 <section id="cta-banner-<?php echo get_row_index(); ?>"
-  class="text-banner <?php the_sub_field('background_color'); ?> <?php echo $text_color; ?> padding-y-100">
-  <span class="badge text-bg-danger">CTA Banner</span>
-  <div class="container">
-    <div class="row ">
+  class="text-banner <?php echo $background_color; ?> <?php echo $text_color; ?> ">
 
-      <div class="col-lg-12">
-        <div class="text-banner-text-content">
-          <?php the_sub_field('cta_banner_content'); ?>
-        </div>
-        <?php if( have_rows('cta_buttons') ): ?>
-        <?php while( have_rows('cta_buttons') ): the_row(); 
+  <?php if( have_rows('background_images') ): ?>
+  <?php while( have_rows('background_images') ): the_row();  
+    $background_image = get_sub_field('background_image');
+  ?>
+  <div class="<?php echo $background_image; ?>">
+    <?php endwhile; ?>
+    <?php endif; ?>
+    <div class="padding-y-100">
+
+      <span class="badge text-bg-danger">CTA Banner</span>
+      <div class="container">
+        <div class="row ">
+
+          <div class="col-lg-12">
+            <div class="text-banner-text-content">
+              <?php the_sub_field('cta_banner_content'); ?>
+            </div>
+            <?php if( have_rows('cta_buttons') ): ?>
+            <?php while( have_rows('cta_buttons') ): the_row(); 
             $button = get_sub_field('cta_button');
             $button_color = get_sub_field('button_color');
             if( $button ):
@@ -28,15 +38,27 @@ if ($background_color == "background-dark-blue" || $background_color == "backgro
               $button_title = $button['title'];
               $button_target = $button['target'] ? $button['target'] : '_self';
           ?>
-        <a href="<?php echo esc_url($button_url); ?>" class="btn <?php echo ($button_color); ?>"
-          target="<?php echo esc_attr($button_target); ?>">
-          <?php echo esc_html($button_title); ?>
-        </a>
-        <?php 
+            <a href="<?php echo esc_url($button_url); ?>" class="btn <?php echo ($button_color); ?>"
+              target="<?php echo esc_attr($button_target); ?>">
+              <?php echo esc_html($button_title); ?>
+            </a>
+            <?php 
             endif;
           endwhile; ?>
-        <?php endif; ?>
+            <?php endif; ?>
+          </div>
+        </div>
       </div>
+
+      <!-- close padding div -->
     </div>
+
+
+
+
+    <?php if( have_rows('background_images') ): ?>
+    <?php while( have_rows('background_images') ): the_row();  ?>
   </div>
+  <?php endwhile; ?>
+  <?php endif; ?>
 </section>
--- inc/logo-banner.php
+++ inc/logo-banner.php
@@ -21,78 +21,97 @@ if ($background_color == "background-dark-blue" || $background_color == "backgro
 ?>
 
 <section id="logo-banner-<?php echo get_row_index(); ?>"
-  class="logo-banner <?php echo $background_color; ?> <?php echo $text_color; ?> padding-y-100"
+  class="logo-banner <?php echo $background_color; ?> <?php echo $text_color; ?>"
   style="--slider-speed: <?php echo $total_duration; ?>s;">
   <span class="badge text-bg-danger">Logo Banner</span>
-  <div class="container gy-4">
-    <div class="row ">
-      <div class="col-lg-12 mb-4">
-        <?php the_sub_field("logo_banner_content"); ?>
-      </div>
-    </div>
-  </div>
 
-  <!-- Single seamless marquee wrapper -->
-  <div class="marquee-wrapper">
-    <div class="animate-marquee d-flex align-items-center">
-      <!-- First set of logos -->
-      <?php if( have_rows('logos') ): ?>
-      <?php while( have_rows('logos') ): the_row(); 
+  <?php if( have_rows('background_images') ): ?>
+  <?php while( have_rows('background_images') ): the_row();  
+    $background_image = get_sub_field('background_image');
+  ?>
+  <div class="<?php echo $background_image; ?>">
+    <?php endwhile; ?>
+    <?php endif; ?>
+    <div class="padding-y-100">
+
+      <div class="container gy-4">
+        <div class="row ">
+          <div class="col-lg-12 mb-4">
+            <?php the_sub_field("logo_banner_content"); ?>
+          </div>
+        </div>
+      </div>
+
+      <!-- Single seamless marquee wrapper -->
+      <div class="marquee-wrapper">
+        <div class="animate-marquee d-flex align-items-center">
+          <!-- First set of logos -->
+          <?php if( have_rows('logos') ): ?>
+          <?php while( have_rows('logos') ): the_row(); 
             $logo = get_sub_field('logo');
             if ( $logo && isset($logo['url']) ):
           ?>
-      <div>
-        <img src="<?php echo esc_url($logo['url']); ?>" alt="<?php echo esc_attr($logo['alt']); ?>">
-      </div>
-      <?php endif; endwhile; ?>
-      <?php endif; ?>
+          <div>
+            <img src="<?php echo esc_url($logo['url']); ?>" alt="<?php echo esc_attr($logo['alt']); ?>">
+          </div>
+          <?php endif; endwhile; ?>
+          <?php endif; ?>
 
-      <!-- Second set (duplicate) -->
-      <?php if( have_rows('logos') ): ?>
-      <?php while( have_rows('logos') ): the_row(); 
+          <!-- Second set (duplicate) -->
+          <?php if( have_rows('logos') ): ?>
+          <?php while( have_rows('logos') ): the_row(); 
           $logo = get_sub_field('logo');
           if ( $logo && isset($logo['url']) ):
         ?>
-      <div>
-        <img src="<?php echo esc_url($logo['url']); ?>" alt="<?php echo esc_attr($logo['alt']); ?>">
-      </div>
-      <?php endif; endwhile; ?>
-      <?php endif; ?>
+          <div>
+            <img src="<?php echo esc_url($logo['url']); ?>" alt="<?php echo esc_attr($logo['alt']); ?>">
+          </div>
+          <?php endif; endwhile; ?>
+          <?php endif; ?>
 
-      <!-- Second set (duplicate) -->
-      <?php if( have_rows('logos') ): ?>
-      <?php while( have_rows('logos') ): the_row(); 
+          <!-- Second set (duplicate) -->
+          <?php if( have_rows('logos') ): ?>
+          <?php while( have_rows('logos') ): the_row(); 
           $logo = get_sub_field('logo');
           if ( $logo && isset($logo['url']) ):
         ?>
-      <div>
-        <img src="<?php echo esc_url($logo['url']); ?>" alt="<?php echo esc_attr($logo['alt']); ?>">
-      </div>
-      <?php endif; endwhile; ?>
-      <?php endif; ?>
+          <div>
+            <img src="<?php echo esc_url($logo['url']); ?>" alt="<?php echo esc_attr($logo['alt']); ?>">
+          </div>
+          <?php endif; endwhile; ?>
+          <?php endif; ?>
 
-      <!-- Second set (duplicate) -->
-      <?php if( have_rows('logos') ): ?>
-      <?php while( have_rows('logos') ): the_row(); 
+          <!-- Second set (duplicate) -->
+          <?php if( have_rows('logos') ): ?>
+          <?php while( have_rows('logos') ): the_row(); 
           $logo = get_sub_field('logo');
           if ( $logo && isset($logo['url']) ):
         ?>
-      <div>
-        <img src="<?php echo esc_url($logo['url']); ?>" alt="<?php echo esc_attr($logo['alt']); ?>">
+          <div>
+            <img src="<?php echo esc_url($logo['url']); ?>" alt="<?php echo esc_attr($logo['alt']); ?>">
+          </div>
+          <?php endif; endwhile; ?>
+          <?php endif; ?>
+
+        </div>
       </div>
-      <?php endif; endwhile; ?>
+
+      <?php if (get_sub_field("after_logo_banner_content")): ?>
+      <div class="container gy-4">
+        <div class="row">
+          <div class="col-lg-12 mt-4">
+            <?php the_sub_field("after_logo_banner_content"); ?>
+          </div>
+        </div>
+      </div>
       <?php endif; ?>
 
+      <!-- close padding div -->
     </div>
-  </div>
 
-  <?php if (get_sub_field("after_logo_banner_content")): ?>
-  <div class="container gy-4">
-    <div class="row">
-      <div class="col-lg-12 mt-4">
-        <?php the_sub_field("after_logo_banner_content"); ?>
-      </div>
-    </div>
+    <?php if( have_rows('background_images') ): ?>
+    <?php while( have_rows('background_images') ): the_row();  ?>
   </div>
+  <?php endwhile; ?>
   <?php endif; ?>
 </section>
