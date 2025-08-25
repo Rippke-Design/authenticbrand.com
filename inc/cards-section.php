8b4dfa4bacd9ab28a6aa176dab056f60fbafd984
Niki Brown <284173+nikibrown@users.noreply.github.com>
2025-08-25 10:10:11 -0700

    CPT for team members, consultants, update cards section, add background images

--- inc/cards-section.php
+++ inc/cards-section.php
@@ -11,51 +11,87 @@ if ($background_color == "background-dark-blue" || $background_color == "backgro
 <section id="cards-section-<?php echo get_row_index(); ?>"
   class="cards-section <?php the_sub_field('background_color'); ?> <?php echo $text_color; ?> background-pattern-cubes background-pattern-right padding-y-100">
 
-  <span class="badge text-bg-danger">Cards Section</span>
-  <div class="container">
-    <div class="row">
-      <?php if (get_sub_field('headline')): ?>
-      <div class="col-lg-12 mb-5">
-        <?php the_sub_field('headline'); ?>
-      </div>
-      <?php endif; ?>
+  <?php if( have_rows('background_images') ): ?>
+  <?php while( have_rows('background_images') ): the_row();  
+    $background_image = get_sub_field('background_image');
+  ?>
+  <div class="<?php echo $background_image; ?>">
+    <?php endwhile; ?>
+    <?php endif; ?>
+    <div class="padding-y-100">
+
+      <span class="badge text-bg-danger">Cards Section</span>
+      <div class="container">
+        <div class="row">
+          <?php if (get_sub_field('headline')): ?>
+          <div class="col-lg-12 mb-5">
+            <?php the_sub_field('headline'); ?>
+          </div>
+          <?php endif; ?>
 
 
-      <?php if (get_sub_field('card_source_selection_type') == 'manual'): ?>
-      <!-- manual card selection -->
-      <?php
+          <?php if (get_sub_field('card_source_selection_type') == 'manual'): ?>
+          <!-- manual card selection -->
+          <?php
           $cards = get_sub_field('cards');
           if ($cards) :
             foreach ($cards as $post) :
               setup_postdata($post);
               $featured_image = get_the_post_thumbnail_url($post->ID, 'large');
               ?>
-      <div class="col-lg-4 mb-5">
-        <div class="card success-story-card">
-          <?php if ($featured_image) : ?>
-          <img src="<?php echo esc_url($featured_image); ?>" alt="<?php echo esc_attr(get_the_title($post->ID)); ?>"
-            class="card-img-top">
-          <?php endif; ?>
-          <div class="card-body d-flex flex-column justify-content-between">
-            <div>
-              <p><?php echo get_the_date('', $post->ID); ?></p>
-              <h3><?php echo esc_html(get_the_title($post->ID)); ?></h3>
+
+
+          <?php if (get_post_type($post->ID) == 'team-member' || get_post_type($post->ID) == 'consultant'): ?>
+          <div class="col-lg-3 mb-5">
+            <div class="card team-member-card h-100">
+              <?php $headshot = get_field('headshot', $post->ID);
+                $name = get_field('name', $post->ID);
+                $title = get_field('title', $post->ID);
+                $linkedin = get_field('linkedin_url', $post->ID);
+                $bio = get_field('bio', $post->ID);
+              ?>
+              <?php if ($headshot && isset($headshot['url'])) : ?>
+              <img class="card-img-top" src="<?php echo esc_url($headshot['url']); ?>"
+                alt="<?php echo esc_attr(get_the_title($post->ID)); ?>" />
+              <?php endif; ?>
+              <div class="card-body text-center">
+                <h3><?php echo $name ?></h3>
+                <p class="text-uppercase"><small><?php echo $title;?></small></p>
+                <a href="<?php echo $linkedin; ?>" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+              </div>
             </div>
-
-            <a href="<?php echo esc_url(get_permalink($post->ID)); ?>" class="">Read More &raquo;</a>
           </div>
-        </div>
-      </div>
-      <?php endforeach; wp_reset_postdata(); ?>
-      <?php endif; ?>
+          <?php else: ?>
+
+          <div class="col-lg-4 mb-5">
+            <div class="card success-story-card">
+              <?php if ($featured_image) : ?>
+              <img src="<?php echo esc_url($featured_image); ?>" alt="<?php echo esc_attr(get_the_title($post->ID)); ?>"
+                class="card-img-top">
+              <?php endif; ?>
+              <div class="card-body d-flex flex-column justify-content-between">
+                <div>
+                  <p><?php echo get_the_date('', $post->ID); ?></p>
+                  <h3><?php echo esc_html(get_the_title($post->ID)); ?></h3>
+                </div>
+
+                <a href="<?php echo esc_url(get_permalink($post->ID)); ?>" class="">Read More &raquo;</a>
+              </div>
+            </div>
+          </div>
+          <?php endif; ?>
+
+
+          <?php endforeach; wp_reset_postdata(); ?>
+          <?php endif; ?>
 
 
 
-      <?php endif; ?>
+          <?php endif; ?>
 
-      <?php if (get_sub_field('card_source_selection_type') == 'automatic'): ?>
-      <!-- automatic card selection -->
-      <?php
+          <?php if (get_sub_field('card_source_selection_type') == 'automatic'): ?>
+          <!-- automatic card selection -->
+          <?php
       $category_id = get_sub_field('card_source');
       $posts = get_posts(array(
         'category' => $category_id,
@@ -63,34 +99,43 @@ if ($background_color == "background-dark-blue" || $background_color == "backgro
       ));
       ?>
 
-      <?php if ($posts) : ?>
-      <?php foreach ($posts as $post) : ?>
-      <?php setup_postdata($post); 
+          <?php if ($posts) : ?>
+          <?php foreach ($posts as $post) : ?>
+          <?php setup_postdata($post); 
       $featured_image = get_the_post_thumbnail_url($post->ID, 'large'); ?>
 
-      <div class="col-lg-4 mb-5">
-        <div class="card success-story-card">
-          <?php if ($featured_image) : ?>
-          <img src="<?php echo esc_url($featured_image); ?>" alt="<?php echo esc_attr(get_the_title($post->ID)); ?>"
-            class="card-img-top">
-          <?php endif; ?>
-          <div class="card-body d-flex flex-column justify-content-between">
-            <div>
-              <p><?php echo get_the_date('', $post->ID); ?></p>
-              <h3><?php echo esc_html(get_the_title($post->ID)); ?></h3>
+          <div class="col-lg-4 mb-5">
+            <div class="card success-story-card">
+              <?php if ($featured_image) : ?>
+              <img src="<?php echo esc_url($featured_image); ?>" alt="<?php echo esc_attr(get_the_title($post->ID)); ?>"
+                class="card-img-top">
+              <?php endif; ?>
+              <div class="card-body d-flex flex-column justify-content-between">
+                <div>
+                  <p><?php echo get_the_date('', $post->ID); ?></p>
+                  <h3><?php echo esc_html(get_the_title($post->ID)); ?></h3>
+                </div>
+
+                <a href="<?php echo esc_url(get_permalink($post->ID)); ?>" class="">Read More &raquo;</a>
+              </div>
             </div>
-
-            <a href="<?php echo esc_url(get_permalink($post->ID)); ?>" class="">Read More &raquo;</a>
           </div>
-        </div>
-      </div>
-      <?php endforeach; wp_reset_postdata(); ?>
-      <?php endif; ?>
-      <?php endif; ?>
+          <?php endforeach; wp_reset_postdata(); ?>
+          <?php endif; ?>
+          <?php endif; ?>
 
+        </div>
+
+      </div>
+
+      <!-- close padding div -->
     </div>
 
+    <?php if( have_rows('background_images') ): ?>
+    <?php while( have_rows('background_images') ): the_row();  ?>
   </div>
+  <?php endwhile; ?>
+  <?php endif; ?>
 </section>
 
 
