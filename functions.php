38510b557a2aebc5d9b1f32534b1b91b5938ea64
Niki Brown <284173+nikibrown@users.noreply.github.com>
2025-09-08 09:39:27 -0700

    disable comments

--- functions.php
+++ functions.php
@@ -388,3 +388,15 @@ function fix_svg_admin_display() {
     </style>';
 }
 add_action('admin_head', 'fix_svg_admin_display');
+
+// Disable support for comments and trackbacks in post types
+function disable_comments_post_types_support() {
+    $post_types = get_post_types();
+    foreach ($post_types as $post_type) {
+        if (post_type_supports($post_type, 'comments')) {
+            remove_post_type_support($post_type, 'comments');
+            remove_post_type_support($post_type, 'trackbacks');
+        }
+    }
+}
+add_action('admin_init', 'disable_comments_post_types_support');
